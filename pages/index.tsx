4634fa95f7bf18b51ae78206a876de136d8cedd2
asadbek0512 <khusanovasadbek777@gmail.com>
2025-06-27 23:49:07 +0900

    feat: Homepage  aos animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { NextPage } from 'next';
 import useDeviceDetect from '../libs/hooks/useDeviceDetect';
 import withLayoutMain from '../libs/components/layout/LayoutHome';
@@ -11,6 +12,8 @@ import Advertisement from '../libs/components/homepage/Advertisement';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import CategoryCards from '../libs/components/homepage/CategoryCard';
 import FlashSale from '../libs/components/homepage/PopularProperties';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export const getStaticProps = async ({ locale }: any) => ({
 	props: {
@@ -21,29 +24,72 @@ export const getStaticProps = async ({ locale }: any) => ({
 const Home: NextPage = () => {
 	const device = useDeviceDetect();
 
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			AOS.init({
+				duration: 1200,
+				easing: 'ease-in-out',
+				once: true, // ❗ faqat bir marta animatsiya qilish
+				offset: 50,
+				delay: 0,
+				anchorPlacement: 'top-bottom',
+				disable: false,
+			});
+
+			// DOM to'liq yuklangach elementlarni aniqlash uchun refresh
+			setTimeout(() => {
+				AOS.refresh();
+			}, 1000);
+		}
+	}, []);
+
 	if (device === 'mobile') {
 		return (
-			<Stack className={'home-page'}>
-				<TrendProperties />
-				<Advertisement />
-				<TopProperties />
-				<TopAgents />
-			</Stack>
-		);
-	} else {
-		return (
-			<Stack className={'home-page'}>
-				<CategoryCards />
-				<TrendProperties />
-				<FlashSale />
-				<Advertisement />
-				<TopProperties />
-				<TopAgents />
-				<Events />
-				<CommunityBoards />
+			<Stack className="home-page">
+				<div data-aos="fade-up">
+					<TrendProperties />
+				</div>
+				<div data-aos="fade-up">
+					<Advertisement />
+				</div>
+				<div data-aos="fade-up">
+					<TopProperties />
+				</div>
+				<div data-aos="fade-up">
+					<TopAgents />
+				</div>
 			</Stack>
 		);
 	}
+
+	return (
+		<Stack className="home-page">
+			<div data-aos="fade-up">
+				<CategoryCards />
+			</div>
+			<div data-aos="fade-up">
+				<TrendProperties />
+			</div>
+			<div data-aos="fade-up">
+				<FlashSale />
+			</div>
+			<div data-aos="fade-up">
+				<Advertisement />
+			</div>
+			<div data-aos="fade-up">
+				<TopProperties />
+			</div>
+			<div data-aos="fade-up">
+				<TopAgents />
+			</div>
+			<div data-aos="fade-up">
+				<Events />
+			</div>
+			<div data-aos="fade-up">
+				<CommunityBoards />
+			</div>
+		</Stack>
+	);
 };
 
 export default withLayoutMain(Home);
