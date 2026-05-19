41da70e265c2d070534724379333e3606af30a9f
asadbek0512 <khusanovasadbek777@gmail.com>
2026-05-19 17:17:36 +0900

    fix: add GET_MY_PROFILE to LayoutHome so homepage shows login state after refresh

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -6,7 +6,7 @@ import { Stack } from '@mui/material';
 import FiberContainer from '../common/FiberContainer';
 // import HeaderFilter from '../homepage/HeaderFilter';
 import { userVar } from '../../../apollo/store';
-import { useReactiveVar } from '@apollo/client';
+import { useQuery, useReactiveVar } from '@apollo/client';
 import { getJwtToken, updateUserInfo } from '../../auth';
 import Chat from '../Chat';
 import AiChat from '../AiChat';
@@ -18,17 +18,48 @@ import Footer from '../Footer';
 import HeaderFilter from '../homepage/HeaderFilter';
 import HeroSection from '../common/HeroSection';
 import SalesToast from '../common/SalesToast';
+import { GET_MY_PROFILE } from '../../../apollo/user/query';
 
 const withLayoutMain = (Component: any) => {
 	return (props: any) => {
 		const device = useDeviceDetect();
 		const user = useReactiveVar(userVar);
 
-		/** LIFECYCLES **/
+		const { data: myProfileData } = useQuery(GET_MY_PROFILE, {
+			fetchPolicy: 'network-only',
+			onError: () => {},
+		});
+
 		useEffect(() => {
-			const jwt = getJwtToken();
-			if (jwt) updateUserInfo(jwt);
-		}, []);
+			if (myProfileData?.getMyProfile) {
+				const member = myProfileData.getMyProfile;
+				userVar({
+					_id: member._id ?? '',
+					memberType: member.memberType ?? '',
+					memberStatus: member.memberStatus ?? '',
+					memberAuthType: member.memberAuthType ?? '',
+					memberPhone: member.memberPhone ?? '',
+					memberNick: member.memberNick ?? '',
+					memberFullName: member.memberFullName ?? '',
+					memberImage: member.memberImage || '/img/profile/defaultUser.svg',
+					memberAddress: member.memberAddress ?? '',
+					memberDesc: member.memberDesc ?? '',
+					memberProperties: member.memberProperties ?? 0,
+					memberRank: member.memberRank ?? 0,
+					memberEmail: member.memberEmail ?? '',
+					memberArticles: member.memberArticles ?? 0,
+					memberPoints: member.memberPoints ?? 0,
+					memberFollowers: member.memberFollowers ?? 0,
+					memberFollowings: member.memberFollowings ?? 0,
+					memberLikes: member.memberLikes ?? 0,
+					memberViews: member.memberViews ?? 0,
+					memberWarnings: member.memberWarnings ?? 0,
+					memberBlocks: member.memberBlocks ?? 0,
+					memberTelegramId: member.memberTelegramId ?? '',
+					memberGoogleId: member.memberGoogleId ?? '',
+				});
+			}
+		}, [myProfileData]);
 
 		/** HANDLERS **/
 
